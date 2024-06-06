08971642d7ba9c7de684f1bb8c9556afd944cf1f
IQBAL CAHYO GUMILAR <iqbalung@gmail.com>
2024-06-06 16:20:29 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const { parsed: localEnv } = require("dotenv").config();
 module.exports = {
   publicRuntimeConfig: {
     CONTACT_EMAIL: localEnv && localEnv.CONTACT_EMAIL,
-    SITE_NAME: localEnv && localEnv.SITE_NAME,
-    DEFAULT_DOMAIN: 'Sorkale',
+    SITE_NAME: 'Sorkale',
+    DEFAULT_DOMAIN: localEnv && localEnv.DEFAULT_DOMAIN,
     RECAPTCHA_SITE_KEY: localEnv && localEnv.RECAPTCHA_SITE_KEY,
     REPORT_EMAIL: localEnv && localEnv.REPORT_EMAIL,
     DISALLOW_ANONYMOUS_LINKS: localEnv && localEnv.DISALLOW_ANONYMOUS_LINKS,
